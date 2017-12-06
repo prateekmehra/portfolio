f70821c9315db8d6f3803f821cae29d7f10d850e
prateekmehra <prateekmehra@github.com>
2017-12-06 22:27:58 +0530

    + logo +js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 $(function(){
 	var src = [];
+	src.push('./images/cover.jpg');
+	src.push('./images/blank.jpg');
+	
 	for (var i=0;i<=17;i++)
 		src.push('./images/interiors/i'+i+'.jpg');
 		
 	for (var i=0;i<=4;i++)
 		src.push('./images/graphic/g'+i+'.jpg');
 
-	src.push('./images/interiors/i18.jpg');				
+	src.push('./images/blank.jpg');				
 				
 	for (var i=0;i<=11;i++)
-		src.push('./images/photography/p'+i+'.jpg');	
+		src.push('./images/photography/p'+i+'.jpg');
+
+	src.push('./images/logo.jpg');		
 
 	$('#photobook').onebook(src,{skin:['light','dark'], bgDark:'#1e1c1e', flip:'soft', border:5, pageColor: 'rgb(128, 128, 128)', cesh:true});
 });
