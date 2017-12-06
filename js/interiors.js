59663544f0567f93bde5567c12020901d79e2308
prateekmehra <prateekmehra@github.com>
2017-12-06 18:27:24 +0530

    +cs +js

--- js/interiors.js
+++ js/interiors.js
@@ -1,21 +1,21 @@
-var src = [ '/portfolio/images/interiors/i1.jpg', 
-			'/portfolio/images/interiors/i2.jpg',
-			'/portfolio/images/interiors/i3.jpg', 
-			'/portfolio/images/interiors/i4.jpg',
-			'/portfolio/images/interiors/i5.jpg', 
-			'/portfolio/images/interiors/i6.jpg',
-			'/portfolio/images/interiors/i7.jpg', 
-			'/portfolio/images/interiors/i8.jpg',
-			'/portfolio/images/interiors/i9.jpg', 
-			'/portfolio/images/interiors/i10.jpg',
-			'/portfolio/images/interiors/i11.jpg', 
-			'/portfolio/images/interiors/i12.jpg',
-			'/portfolio/images/interiors/i13.jpg', 
-			'/portfolio/images/interiors/i14.jpg',
-			'/portfolio/images/interiors/i15.jpg', 
-			'/portfolio/images/interiors/i16.jpg',
-			'/portfolio/images/interiors/i17.jpg', 
-			'/portfolio/images/interiors/i18.jpg'
+var src = [ '../images/interiors/i1.jpg', '../images/interiors/i2.jpg',
+			'../images/interiors/i3.jpg', 
+			'../images/interiors/i4.jpg',
+			'../images/interiors/i5.jpg', 
+			'../images/interiors/i6.jpg',
+			'../images/interiors/i7.jpg', 
+			'../images/interiors/i8.jpg',
+			'../images/interiors/i9.jpg', 
+			'../images/interiors/i10.jpg',
+			'../images/interiors/i11.jpg', 
+			'../images/interiors/i12.jpg',
+			'../images/interiors/i13.jpg', 
+			'../images/interiors/i14.jpg',
+			'../images/interiors/i15.jpg', 
+			'../images/interiors/i16.jpg',
+			'../images/interiors/i17.jpg', 
+			'../images/interiors/i18.jpg'
 			];
-var options  = {};
-$('#mybook').onebook(src);
+
+$('#photobook').onebook(src,{skin:['light','dark'], bgDark:'#222222 url(../images/interiors/bg.jpg)', flip:'soft', border:25, cesh:false});
+		
