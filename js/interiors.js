c88115c07ba48cf4ab9094239f112e93b08b5ba1
prateekmehra <prateekmehra@github.com>
2017-12-06 17:01:41 +0530

    + interiors +js

---
+++ js/interiors.js
@@ -0,0 +1,21 @@
+var src = [ '/portfolio/images/interiors/i1.jpg', 
+			'/portfolio/images/interiors/i2.jpg',
+			'/portfolio/images/interiors/i3.jpg', 
+			'/portfolio/images/interiors/i4.jpg',
+			'/portfolio/images/interiors/i5.jpg', 
+			'/portfolio/images/interiors/i6.jpg',
+			'/portfolio/images/interiors/i7.jpg', 
+			'/portfolio/images/interiors/i8.jpg',
+			'/portfolio/images/interiors/i9.jpg', 
+			'/portfolio/images/interiors/i10.jpg',
+			'/portfolio/images/interiors/i11.jpg', 
+			'/portfolio/images/interiors/i12.jpg',
+			'/portfolio/images/interiors/i13.jpg', 
+			'/portfolio/images/interiors/i14.jpg',
+			'/portfolio/images/interiors/i15.jpg', 
+			'/portfolio/images/interiors/i16.jpg',
+			'/portfolio/images/interiors/i17.jpg', 
+			'/portfolio/images/interiors/i18.jpg'
+			];
+
+$('#mybook').onebook(src,{OPTIONS});
