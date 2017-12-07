20c264343cb1ae8a078f54f27f1118df98313577
prateekmehra <prateekmehra@github.com>
2017-12-07 06:51:41 +0530

    +hog

--- js/hog.js
+++ js/hog.js
@@ -1,27 +1,35 @@
-function onYouTubeIframeAPIReady() {
-  var player;
-  player = new YT.Player('pres', {
-    videoId: 'RTu6kkNqxY4', // YouTube Video ID
-    width: 880,               // Player width (in px)
-    height: 316,              // Player height (in px)
-    origin: "https://www.youtube.com",
-    widget_referrer: "https://www.youtube.com",
-    playerVars: {
-      autoplay: 1,        // Auto-play the video on load
-      controls: 1,        // Show pause/play buttons in player
-      showinfo: 0,        // Hide the video title
-      modestbranding: 1,  // Hide the Youtube Logo
-      loop: 1,            // Run the video in a loop
-      fs: 0,              // Hide the full screen button
-      cc_load_policy: 0, // Hide closed captions
-      iv_load_policy: 3,  // Hide the Video Annotations
-      autohide: 0         // Hide video controls when playing
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-    },
-    events: {
-      onReady: function(e) {
-        player.setVolume(1);
-      }
+    var player;
+    function onYouTubeIframeAPIReady() {
+      player = new YT.Player('pres', {
+        height: '390',
+        width: '640',
+        videoId: 'RTu6kkNqxY4',
+        origin: "https://prateekmehra.github.io",
+        widget_referrer: "https://prateekmehra.github.io",
+        playerVars: {
+          autoplay: 1,        // Auto-play the video on load
+          controls: 1,        // Show pause/play buttons in player
+          showinfo: 0,        // Hide the video title
+          modestbranding: 1,  // Hide the Youtube Logo
+          loop: 1,            // Run the video in a loop
+          fs: 0,              // Hide the full screen button
+          cc_load_policy: 0, // Hide closed captions
+          iv_load_policy: 3,  // Hide the Video Annotations
+          autohide: 0         // Hide video controls when playing
+
+        },
+        events: {
+          'onReady': onPlayerReady,
+        }
+      });
     }
-  });
- };
+
+    function onPlayerReady(event) {
+      player.setVolume(1);
+    }
+
