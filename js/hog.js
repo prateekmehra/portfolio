daa04a99a016fc198ca3c16bc98041c7cb502335
prateekmehra <prateekmehra@github.com>
2017-12-07 06:57:41 +0530

    +hog

--- js/hog.js
+++ js/hog.js
@@ -6,18 +6,16 @@
     var player;
     function onYouTubeIframeAPIReady() {
       player = new YT.Player('pres', {
-        height: '390',
-        width: '640',
+        height: '315',
+        width: '400',
         videoId: 'RTu6kkNqxY4',
-        origin: "https://prateekmehra.github.io",
-        widget_referrer: "https://prateekmehra.github.io",
         playerVars: {
           autoplay: 1,        // Auto-play the video on load
           controls: 1,        // Show pause/play buttons in player
           showinfo: 0,        // Hide the video title
           modestbranding: 1,  // Hide the Youtube Logo
           loop: 1,            // Run the video in a loop
-          fs: 0,              // Hide the full screen button
+          fs: 1,              // Hide the full screen button
           cc_load_policy: 0, // Hide closed captions
           iv_load_policy: 3,  // Hide the Video Annotations
           autohide: 0         // Hide video controls when playing
