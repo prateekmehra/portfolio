c99b6d068733331c9658933ab755464548cfaec9
prateekmehra <prateekmehra@github.com>
2017-12-12 15:16:16 +0530

    +go

--- js/go.js
+++ js/go.js
@@ -16,7 +16,7 @@
           modestbranding: 1,  // Hide the Youtube Logo
           loop: 1,            // Run the video in a loop
           fs: 1,              // Hide the full screen button
-          cc_load_policy: 0, // Hide closed captions
+          cc_load_policy: 1, // Hide closed captions
           iv_load_policy: 3,  // Hide the Video Annotations
           autohide: 0         // Hide video controls when playing
 
