0fe29765d0ef2c7c9ec18c6db002915c0e81d986
prateekmehra <prateekmehra@github.com>
2017-12-21 03:30:21 +0530

    + chronos + videos

--- js/chronos.js
+++ js/chronos.js
@@ -1,31 +1,43 @@
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('pres', {
+    videoId: 'G7-st4Wc2aU',
+    playerVars: {
+      autoplay: 1,        // Auto-play the video on load
+      controls: 1,        // Show pause/play buttons in player
+      showinfo: 0,        // Hide the video title
+      modestbranding: 0,  // Hide the Youtube Logo
+      loop: 1,            // Run the video in a loop
+      fs: 1,              // Hide the full screen button
+      cc_load_policy: 1, // Hide closed captions
+      iv_load_policy: 3,  // Hide the Video Annotations
+      autohide: 0         // Hide video controls when playing
+
+    },
+    events: {
+      'onReady': onPlayerReady,
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  player.setVolume(100);
+}
+
 $(function(){
-
-
-	var src = [];
-	for (var i=1;i<=12;i++)
-		src.push('<img class="owl-lazy" data-src="./images/chronos/carousel/chronos abcd-page-00'+i+'.jpg"  style="width:500px" alt="">');
-
-	 $('#pres').append(src);
-	var owl = $('.owl-carousel.owl-theme');
-
-	owl.owlCarousel({
-	items:1,
-	lazyLoad:true,
-	loop:true,
-	margin:10,
-	autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true,
-    nav:true,
-    dotsEach:5
+	$('#image-tray').owlCarousel({
+		items:1,
+		lazyLoad:true,
+		loop:true,
+		autoplay:true,
+		autoplayTimeout:3000,
+		autoplayHoverPause:true,
+		nav:true,
+		dots:false
 	});
-
-	owl.hover(function(){
-		owl.trigger('stop.owl.autoplay')
-	});
-
-	owl.on('mouseout',function(){
-		owl.trigger('play.owl.autoplay',[3000]);
-	});
-
 });
