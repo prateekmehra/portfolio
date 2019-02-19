56770110619b58bac8bf96d770bcdd8ab06b4e69
Prateek Mehra <prateek11078@iiitd.ac.in>
2019-02-19 03:28:20 -0800

    + Added palette swapping for index

--- js/background.js
+++ js/background.js
@@ -1,4 +1,5 @@
 var backgroundArr = ["rgb(238,191,78)","rgb(34, 85, 131)","rgb(93,208,255)","rgb(219, 108, 96)","rgb(0, 179, 111)","rgba(189,110,110,0.7)", "rgba(255, 127, 64, 0.7)","rgba(71, 115, 184, 0.9)"];
+var colorArr = ["rgb(17,17,17)","rgb(255,255,255)"];
 
 function addBackgroundBlur(){
 	$("#palette").addClass("blurred-box");
@@ -11,9 +12,19 @@ function removeBackgroundBlur(){
 function updatePalette(h){
 	if(h>=0){
 		$("#palette").css({"background":backgroundArr[h]});
+		$(".navLink").css({"color":colorArr[h%2]});
+		$("#el_pDo0vxjT-X").css({"fill":colorArr[h%2]});
+		$("#menuToggle span").css({"background":colorArr[h%2]});
+		$(".arrow").css({"color":colorArr[h%2]});
+		$("#go-back").css({"border":"2px solid " + colorArr[h%2]});
 	}
 	else{
 		$("#palette").css({"background":"none"});
+		$(".navLink").css({"color":colorArr[1]});
+		$("#el_pDo0vxjT-X").css({"fill":colorArr[1]});
+		$("#menuToggle span").css({"background":colorArr[1]});
+		$(".arrow").css({"color":colorArr[1]});
+		$("#go-back").css({"border":"2px solid " + colorArr[1]});
 	}
 }
 
