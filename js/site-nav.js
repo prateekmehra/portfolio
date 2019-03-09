175025b55551af03a02dc63862360dec582891b4
Prateek Mehra <prateek11078@iiitd.ac.in>
2019-03-09 02:38:25 -0800

    final nav 5

--- js/site-nav.js
+++ js/site-nav.js
@@ -1,11 +1,13 @@
-$(window).load(function(){
+$(function(){
+	$(window).load(function(){
 
-	var selector = '.'+window.location.pathname.split('/')[1]+'-nav';
-	$("#horizontal-menu").children(selector).each(function( index, element ) {
-	    $(element).css('display','none');
-	});
-	$("#menu").children(selector).each(function( index, element ) {
-	    $(element).css('display','none');
-	});
+		var selector = '.'+window.location.pathname.split('/')[1]+'-nav';
+		$("#horizontal-menu").children(selector).each(function( index, element ) {
+		    $(element).css('display','none');
+		});
+		$("#menu").children(selector).each(function( index, element ) {
+		    $(element).css('display','none');
+		});
 
+	});
 });
