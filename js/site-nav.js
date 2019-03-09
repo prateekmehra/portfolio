e0df370d14318fa899b5c10e534310fd33a2fb69
Prateek Mehra <prateek11078@iiitd.ac.in>
2019-03-09 02:21:15 -0800

    final nav 3

--- js/site-nav.js
+++ js/site-nav.js
@@ -1,4 +1,5 @@
-window.load(function(){
+$(window).load(function(){
+
 	var selector = '.'+window.location.pathname.split('/')[1]+'-nav';
 	$("#horizontal-menu").children(selector).each(function( index, element ) {
 	    $(element).css('display','none');
