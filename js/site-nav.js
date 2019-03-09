2b1ebda6216be2fdffbeceaa1fc0e619e5d36f50
Prateek Mehra <prateek11078@iiitd.ac.in>
2019-03-09 01:48:14 -0800

    test

--- js/site-nav.js
+++ js/site-nav.js
@@ -1,3 +1,6 @@
-console.log(window.location.pathname.split('/')[1]);
-var selector = '#'+window.location.pathname.split('/')[1]+'-nav';
-$(selector).css('display', 'none');
+$(function(){
+	console.log(window.location.pathname.split('/')[1]);
+	var selector = '#'+window.location.pathname.split('/')[1]+'-nav';
+	console.log($(selector));
+	$(selector).css('display', 'none');
+});
