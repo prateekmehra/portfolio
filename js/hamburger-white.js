725f7d7231fddc168df5d65c854632d5ee235497
Prateek Mehra <prateek11078@iiitd.ac.in>
2019-11-23 18:10:36 -0800

    + hamburger-white

---
+++ js/hamburger-white.js
@@ -0,0 +1,30 @@
+
+$(function(){
+	$(window).load(function(){
+		$("#menuToggle").click(function(){
+			if ($("#menuToggle").children(':input').is(':checked')){
+				$("#menu-overlay").removeClass('hidden').addClass('visible');
+				$('body').addClass('noscroll');
+				$('html').addClass('noscroll');
+				$("#menu-overlay").show();
+				/* color logo and arrow white over hamburger overlay */
+				$("#menuToggle span").css({"background":"rgb(0,0,0)"});
+			}
+			else{
+				$("#menu-overlay").removeClass('visible').addClass('hidden');
+				$('body').removeClass('noscroll');
+				$('html').removeClass('noscroll');
+				$("#menuToggle span").css({"background":"rgb(0,0,0)"});
+			}
+		});
+	});
+});
+
+function rgb2hex(rgb){
+ rgb = rgb.match(/^rgb((d+),s*(d+),s*(d+))$/);
+ console.log(rgb);
+ return "#" +
+  ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
+  ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
+  ("0" + parseInt(rgb[3],10).toString(16)).slice(-2);
+}
